fc41f467806f6df831af674cb3649a996e8d1030
Rohit Yadav <dev@rowhit.me>
2023-12-04 19:08:30 -0500

    Further updated the install script

--- script__ry__basic_user_environment_setup__20231204.sh
+++ script__ry__basic_user_environment_setup__20231204.sh
@@ -3,23 +3,24 @@
 # notes
 # ******************************************************************************
 # purpose: setup a basic environment with all the tools
+# run as user: root
 # ******************************************************************************
 # main
 # ******************************************************************************
 # install all desired packages
 # ==============================================================================
 # apt packages
-sudo apt install -y wget curl vim python3 fzf ripgrep tmux
+apt install -y wget curl vim python3 fzf ripgrep tmux
 
 #create scratch dir and cd into it
-mkdir ~/scratch && cd ~/scratch
+mkdir -p /root/scratch && cd /root/scratch
 
 # install starship
 curl -sS https://starship.rs/install.sh | sh
 
 # install zoxide
 wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.2/zoxide_0.9.2_amd64.deb
-sudo apt install ./zoxide_0.9.2_amd64.deb
+apt install ./zoxide_0.9.2_amd64.deb
 
 download_url="https://raw.githubusercontent.com/rowhit/ry__env_setup__20231204/main"
 # configure bashrc
