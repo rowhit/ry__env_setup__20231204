05187bba406d73c9f99dc563a8b8fe130f59f080
Rohit Yadav <dev@rowhit.me>
2023-12-04 19:02:01 -0500

    Updated the script

--- script__ry__basic_user_environment_setup__20231204.sh
+++ script__ry__basic_user_environment_setup__20231204.sh
@@ -21,15 +21,15 @@ curl -sS https://starship.rs/install.sh | sh
 wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.2/zoxide_0.9.2_amd64.deb
 sudo apt install ./zoxide_0.9.2_amd64.deb
 
+download_url="https://raw.githubusercontent.com/rowhit/ry__env_setup__20231204/main"
 # configure bashrc
-wget -P ~/.bashrc https://github.com/rowhit/ry__env_setup__20231204/blob/main/bashrc
+wget -P ~/.bashrc ${download_url}/bashrc
 
 # configure tmux
-wget -P ~/.tmux.conf https://github.com/rowhit/ry__env_setup__20231204/blob/main/tmux.conf
+wget -P ~/.tmux.conf ${download_url}/tmux.conf
 
 # configure starship
 mkdir -p ~/.config
-wget -P ~/.config/starship.toml https://github.com/rowhit/ry__env_setup__20231204/blob/main/starship.toml
-
+wget -P ~/.config/starship.toml ${download_url}/starship.toml
 
 # ##############################################################################
