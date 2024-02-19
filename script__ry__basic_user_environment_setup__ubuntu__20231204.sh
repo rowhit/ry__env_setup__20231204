1c19b6aa5ec9a3e2701107dfa57e0fce4c130e6b
Rohit Yadav <dev@rowhit.me>
2024-02-19 13:21:11 -0500

    Updated non root user script to setup vim as well

--- script__ry__basic_user_environment_setup__ubuntu__20231204.sh
+++ script__ry__basic_user_environment_setup__ubuntu__20231204.sh
@@ -1,32 +1,37 @@
 #!/usr/bin/env sh
+# * file: script__ry__basic_user_environment_setup__ubuntu__20231204.sh
 # ##############################################################################
-# notes
+# ** notes
 # ******************************************************************************
 # purpose: setup a basic environment with all the tools
 # run as user: ubuntu
 
 
 # ******************************************************************************
-# vars
+# ** vars
 # ******************************************************************************
-
-
-# ******************************************************************************
-# main
-# ******************************************************************************
-# install all desired packages
-# ==============================================================================
-
 download_url="https://raw.githubusercontent.com/rowhit/ry__env_setup__20231204/main"
-# configure bashrc
+
+
+# ******************************************************************************
+# ** main
+# ******************************************************************************
+# *** configure bashrc
 wget -O ~/.bashrc ${download_url}/bashrc
 
-# configure tmux
+# *** configure tmux
 wget -O ~/.tmux.conf ${download_url}/tmux.conf
 
-# configure starship
+# *** configure starship
 mkdir -p ~/.config
 wget -O ~/.config/starship.toml ${download_url}/starship.toml
+
+# *** configure starship
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+wget -O ~/.vimrc ${download_url}/vimrc
+
 
 
 # ##############################################################################
